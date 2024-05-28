5cf2d420f9384b9f00c3f1637767e031cb06cfb4
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-05-27 23:51:45 -0300

    footer

--- src/components/products/ItemPage.jsx
+++ src/components/products/ItemPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import remeraNegra from "../../assets/jpeg/remeraNegra2L.jpg"
-import remeraBlanca from "../../assets/jpeg/remeraBlanca2L.png"
+import remeraNegra from "../../assets/jpeg/remeraNegra2L.jpg";
+import remeraBlanca from "../../assets/jpeg/remeraBlanca2L.png";
 
 const items = [
     { id: 1, title: 'Producto 1', price: 100, image: remeraNegra, description: 'Descripción del Producto 1', sizes: ['S', 'M', 'L'], colors: ['Rojo', 'Azul'] },
@@ -12,9 +12,8 @@ const ItemPage = () => {
     const { id } = useParams();
     const item = items.find(item => item.id === parseInt(id));
     const buttonOnClick = () => {
-
-        console.log('Agregar al carrito')
-    }
+        console.log('Agregar al carrito');
+    };
 
     if (!item) {
         return <div>Producto no encontrado</div>;
@@ -22,23 +21,30 @@ const ItemPage = () => {
 
     return (
         <div className="item-page">
-            <h2>{item.title}</h2>
-            <img src={item.image} alt={item.title} />
-            <p>{item.description}</p>
-            <p>Precio: ${item.price}</p>
-            <div>
-                <h3>Talles disponibles:</h3>
-                <ul>
-                    {item.sizes.map(size => <li key={size}>{size}</li>)}
-                </ul>
+            <div className="container">
+                <div className="image">
+                    <img src={item.image} alt={item.title} />
+                </div>
+                <div className="details">
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
+                    <div>
+                        <h3>Talles disponibles:</h3>
+                        <ul>
+                            {item.sizes.map(size => <li key={size}>{size}</li>)}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Colores disponibles:</h3>
+                        <ul>
+                            {item.colors.map(color => <li key={color}>{color}</li>)}
+                        </ul>
+                    </div>
+                    <div className="price">Precio: ${item.price}</div>
+
+                    <button onClick={buttonOnClick}>Agregar al carrito</button>
+                </div>
             </div>
-            <div>
-                <h3>Colores disponibles:</h3>
-                <ul>
-                    {item.colors.map(color => <li key={color}>{color}</li>)}
-                </ul>
-            </div>
-            <button onClick={buttonOnClick}>Agregar al carrito</button>
         </div>
     );
 };
