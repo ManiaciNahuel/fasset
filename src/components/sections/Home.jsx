24239f8dc2f7008415ee30d91207b82a33ff3296
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2024-06-04 21:36:50 -0300

    Home logo acomodado

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import isotipo from "../../assets/images/Isotipo.jpg"
+import isotipo from "../../assets/images/Isotipo.jpg";
 import { Section } from "./Section";
 
 export const Home = () => {
   return (
     <>
-    <section className="home-hero" id="home">
-      {/*  <!-- HELLO SECTION --> */}
-      <div className="home-hero__content">
-        <h1 className="heading-primary">Bienvenido a FASSET!</h1>
-        <img src={isotipo} alt="isotipo" className="home-hero__logo"/>
-      </div>
+      <section className="home-hero" id="home">
+        <div className="home-hero__content">
+          <h1 className="heading-primary">Bienvenido a FASSET!</h1>
+          <img src={isotipo} alt="isotipo" className="home-hero__logo" />
+        </div>
         <div className="home-hero__boton">
-          <a href="./#section" className="">
+          <a href="./#section">
             DROP 1
           </a>
         </div>
-      <div className="home-hero__mouse-scroll-cont">
-        <div className="mouse"></div>
-      </div>
-    </section>
-      <Section></Section>
+      </section>
+      <Section />
     </>
   );
 };
