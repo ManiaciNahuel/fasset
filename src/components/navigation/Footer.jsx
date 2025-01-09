648d53cf4e7ee99430cab28ed351ba8cf46fd5bf
Nahuel Maniaci <nahuelmaniaci@gmail.com>
2025-01-08 21:43:36 -0300

    imgs y footer

--- src/components/navigation/Footer.jsx
+++ src/components/navigation/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../assets/images/Logotipo.jpg"
 import instagram from "../../assets/png/insta-ico.png"
-import mail from "../../assets/png/icons8-mail-100.png"
+import wsp from "../../assets/png/whatsapp-ico.png"
 
 export const Footer = () => {
   return (
@@ -9,32 +9,36 @@ export const Footer = () => {
       <div className="main-container">
         <div className="main-footer__upper">
           <div className="main-footer__row main-footer__row-1">
-           
+
             <div className="main-footer__social-cont">
               {/* Instagram */}
-              <a target="_blank" rel="noreferrer" href="https://instagram.com">
+              <a target="_blank" rel="noreferrer" href="https://www.instagram.com/fasset.ar">
                 <img
                   src={instagram}
                   alt="Logo"
                   className="main-footer__icon"
                 />
               </a>
-              {/* Gmail */}
-              <a target="_blank" rel="noreferrer" href="https://gmail.com">
+              {/* Instagram */}
+              <a target="_blank" rel="noreferrer" href="https://wa.me/+5493512185195">
                 <img
-                  src={mail}
+                  src={wsp}
                   alt="Logo"
                   className="main-footer__icon"
                 />
               </a>
             </div>
+            <div className="main-footer__mail">
+              fasset.argentina@gmail.com
+              
+            </div>
             <a rel="noreferrer" href="./#">
-                <img
-                  src={logo}
-                  alt="Logo"
-                  className="main-footer__logo-img"
-                />
-              </a>
+              <img
+                src={logo}
+                alt="Logo"
+                className="main-footer__logo-img"
+              />
+            </a>
           </div>
           <div className="main-footer__row main-footer__row-2">
             <h4 className="heading heading-sm text-lt">¿Quienes somos?</h4>
